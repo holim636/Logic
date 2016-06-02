04f71d7779a511f33510647c51b4b592155d06e8
ParkKwangWoo <qkreheh104@naver.com>
2016-06-02 12:58:46 +0900

    비트맵 수

--- Logic/Resource.h
+++ Logic/Resource.h
@@ -5,12 +5,12 @@
 #define IDD_ABOUTBOX                    100
 #define IDR_MAINFRAME                   128
 #define IDR_LogicTYPE                   130
-#define IDB_BITMAP1                     310
-#define IDB_BITMAP2                     312
 #define IDB_BITMAP3                     313
 #define IDB_BITMAP4                     314
 #define IDB_BITMAP5                     315
 #define IDB_BITMAP6                     316
+#define IDB_BITMAP1                     310
+#define IDB_BITMAP2                     321
 #define ID_32771                        32771
 #define ID_32772                        32772
 #define ID_32773                        32773
@@ -23,7 +23,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        317
+#define _APS_NEXT_RESOURCE_VALUE        322
 #define _APS_NEXT_COMMAND_VALUE         32778
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
